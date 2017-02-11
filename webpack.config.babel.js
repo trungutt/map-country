042b6dd9fa381e2e986e1fc3773ca86e474d71e7
trungutt <trung.nguyen@adyoulike.com>
2017-02-11 09:35:42 +0100

    chore(setup): webpack hot reload + test + flow

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,32 +1,48 @@
 import path from 'path';
 import webpack from 'webpack';
 
-
-const devtool = 'cheap-module-eval-source-map';
+const devtool = 'inline-source-map';
 const entry = [
-	'webpack-hot-middleware/client',
-	'./src/index',
+	'webpack-dev-server/client?http://localhost:8080',
+	'webpack/hot/only-dev-server',
+	path.join(__dirname, 'src/index'),
 ];
 const output = {
-	path: path.join(__dirname, 'dist'),
+	path: __dirname,
 	filename: 'bundle.js',
-	publicPath: '/static/',
+	publicPath: '/',
 };
-const plugins = [
-	new webpack.HotModuleReplacementPlugin(),
-];
-const scriptsLoader = {
-	test: /\.js$/,
-	loaders: ['react-hot-loader', 'babel-loader'],
-	include: path.join(__dirname, 'src'),
+const devServer = {
+	contentBase: path.resolve(__dirname, 'src'),
+	quiet: false,
+	noInfo: false,
+	stats: {
+		assets: false,
+		colors: true,
+		version: false,
+		hash: false,
+		timings: false,
+		chunks: false,
+		chunkModules: false,
+	},
+};
+const scriptLoader = {
+	loader: 'babel-loader',
+	include: path.resolve(__dirname, 'src'),
+	exclude: /node_modules/,
+	test: /\.jsx$|\.js$/,
+};
+const plugins = [new webpack.HotModuleReplacementPlugin()];
+const resolve = {
+	extensions: ['.js', '.jsx'],
 };
 
 export default {
 	devtool,
 	entry,
 	output,
+	devServer,
+	module: { loaders: [scriptLoader] },
 	plugins,
-	module: {
-		loaders: [scriptsLoader],
-	},
+	resolve,
 };
